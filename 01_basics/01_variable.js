79f0cf07cf922de6c2544fbf21657ec2ba43aaac
Kazi-Irfanul-Islam <130597597+Kazi-Irfanul-Islam@users.noreply.github.com>
2025-06-27 10:09:42 +0000

    variable in js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountID = 123;
+let accountEmail = "irfanulislam01851@gmail.com";
+var accountPassword = "12345"
+accountCity = "Feni";
+let accountStatus
+console.log(accountID);
+
+/* 
+prefer not to use var of issues in block scope and functional scope
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountStatus]);
