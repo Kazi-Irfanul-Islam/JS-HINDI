9a1cce0bf612b172ac1cf66cdeb6a69e372cb6ce
Kazi-Irfanul-Islam <130597597+Kazi-Irfanul-Islam@users.noreply.github.com>
2025-06-27 10:33:34 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+// "use strict" // treat all JS code as newer version
+
+// alert("Hello") // we are using nodeJS not brower
+
+// javaScript Documentation
+// ECAMSCRIPT
+// MDN DOCS
+
+
+let name = "payel"
+let age = 22
+let isLoggedIn = false;
+
+// number -> range upto 2^52
+// bigInt
+// string -> ""
+// boolean -> true or false
+// null -> standalone value
+// undefined -> not defined
+// symbol -> unique
+
+// object
+
+console.log(typeof "payel");
+console.log(typeof null);
+console.log(typeof undefined);
