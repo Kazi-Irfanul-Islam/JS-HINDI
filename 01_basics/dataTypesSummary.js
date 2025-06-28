226faa7bae6dfa8fa80e5d652756788138bde397
Kazi-Irfanul-Islam <130597597+Kazi-Irfanul-Islam@users.noreply.github.com>
2025-06-28 10:47:11 +0000

    Datatypes Overview

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,44 @@
+// Primitive data Types
+// 7 types 
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Reference (Non primitive) data Type
+// Array, Objects, Functions
+
+// * Premitive Datatypes
+
+//                   Type                                   typeof
+
+// i)               Number                                    number
+// ii)              String                                    string
+// iii)             Boolean                                   boolean
+// iv)              Bigint                                     bigint
+// v)               Symbol                                     symbol
+// vi)              Null                                       object
+// vii)             Undefined                                  undefined
+
+
+// * Non-Premitive OR Referance OR Object datatype
+
+//                   Type                                   typeof
+
+// i)               Object                                  object
+// ii)              Array                                   object
+// iii)             Function                                function(object)
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+const bigNumber = 2345678765434567876542345434543234323433243534343343343324324343343534n
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+const heros = ["irfanul", "islam", "payel"];
+
+const myFunction = function(){
+    console.log("Hello World ! this is a function in JavaScript");
+}
+
+console.log(typeof myFunction);
+myFunction(); // printing function
